0d1feeef52b07f745dbee54a61cbe497e2ed4316
Eric Pattison <epattison@bastiongames.com>
2017-07-04 07:24:49 -0600

    finished first working pass of iOS project

--- keep/window/OpenGLWindow.ios.cpp
+++ keep/window/OpenGLWindow.ios.cpp
@@ -1,9 +1,38 @@
-//
-//  OpenGLWindow.ios.cpp
-//  citadel
-//
-//  Created by Eric Pattison on 7/3/17.
-//  Copyright © 2017 Eric Pattison. All rights reserved.
-//
+#include "OpenGLWindow.h"
+#include "../../watchtower/device/OpenGL/OpenGLDevice.h"
 
-#include <stdio.h>
+class OpenGLWindow::Impl {
+public:
+    Impl(WindowInfo info) {
+        windowInfo = info;
+    }
+    WindowInfo& getWindowInfo() { return windowInfo; }
+private:
+    WindowInfo windowInfo;
+};
+
+OpenGLWindow::OpenGLWindow(WindowInfo& info) : SystemWindow(info) {
+    impl = MakeUPtr<Impl>(info);
+}
+
+OpenGLWindow::~OpenGLWindow() {
+    
+}
+
+void OpenGLWindow::Resize(u32 width, u32 height) {
+
+}
+
+void OpenGLWindow::Title(String title) {
+
+}
+
+WindowStatus OpenGLWindow::ProcessMessages() {
+    return WindowStatus::GameUpdate;
+}
+
+void OpenGLWindow::Exit(int errorCode) { }
+
+SPtr<Device> OpenGLWindow::AcquireDevice() {
+    return MakeSPtr<OpenGLDevice>(impl->getWindowInfo());
+}
